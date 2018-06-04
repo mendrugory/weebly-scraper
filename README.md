1ceb5043441de127ed0c017b0b8eb32c2e178f59
gonzalo <gj@palmtreestatistics.com>
2018-06-04 16:56:51 +0200

    Updates to scrap weebly and the images

--- README.md
+++ README.md
@@ -4,11 +4,15 @@ Weebly provides the option to "back up" a site, but not the actual posts or cont
 
 This is a very simple script to scrape a weebly site's blog posts into markdown files that can be used in things like Hugo or Jekyll, or just be viewed by hand. To import markdown files to Wordpress see [this link](https://tyler.io/importing-jekyll-posts-into-wordpress/).
 
-To use run this script with python on the command line with the first argument being the website address (the weebly.com version) and the second being the number of pages of blog posts to attempt to scrape (if you don't know how many pages try a large number):
+To use run this script with python on the command line with the first argument being the website address (the weebly.com version) and the second being the target folder:
 
 ```shell
-python3 weebly-scraper.py http://example.weebly.com/ 20
+python3 weebly-scraper.py http://example.weebly.com ./content/
 ```
 
 Requirements: [Python 3](https://wiki.python.org/moin/BeginnersGuide/Download
 ), [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup)
+
+```shell
+pip3 install -r requirements.txt
+```
--- weebly-scrapy.py
+++ weebly-scrapy.py
@@ -3,7 +3,9 @@
 
 
 import sys
+import os
 import datetime
+import urllib.request
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -14,11 +16,12 @@ DATE_FORMAT = 'en'  # Put 'fr' here if your dates fit the format '%d/%m/%Y'
 
 class WeeblyScraper():
 
-    def scrape(self, weebly, nbPages):
+    def scrape(self, weebly, target_folder):
         post_urls = []
+        page_number = 1
 
-        for pageNumber in range(1, abs(int(nbPages)) + 1):
-            address = weebly + 'previous/' + str(pageNumber)
+        while True:
+            address = weebly + '/blog/previous/' + str(page_number)
             print("Scraping page (" + address + ')')
 
             try:
@@ -27,12 +30,18 @@ class WeeblyScraper():
 
                 if soup.find(id='blogTable') is not None:
                     for post in soup.findAll('a', {'class': 'blog-title-link'}):
-                        post_urls.append(post.get('href'))
+                        formatted_url = "http:{}".format(post.get('href'))
+                        post_urls.append(formatted_url)
+
+                page_number += 1
 
             except:
-                print("Page not found, you don\'t have that many posts.")
+                break
+
+        print(post_urls)
 
         for post_url in post_urls:
+            print(post_url)
             url = urlopen(post_url)
             soup = BeautifulSoup(url.read(), 'html.parser')
 
@@ -41,23 +50,38 @@ class WeeblyScraper():
 
             date = soup.findAll('p', {'class': 'blog-date'})
             date = date[0].get_text().encode('utf-8', 'ignore').strip()
-            date = datetime.datetime.strptime(date.decode('utf-8'), '%d/%m/%Y' if DATE_FORMAT == 'fr' else '%m/%d/%Y').strftime('%Y-%m-%d')
+            date = datetime.datetime.strptime(date.decode('utf-8'), '%d/%m/%Y').strftime('%Y-%m-%d')
 
             content = soup.findAll('div', {'class': 'blog-content'})
             content = content[0].prettify().encode('utf-8', 'ignore')
 
             url = post_url.replace(weebly, '').encode('utf-8', 'ignore')
             filename = url.decode('utf-8').replace('articles/', '')[:MAX_FILE_NAME]
+            file_name = (target_folder + date + '-' + filename + '.md').replace("-/blog/", "-blog__")
             print("... writing " + date + '-' + filename + '.md')
-            post_md = open('content/' + date + '-' + filename + '.md', 'w+')
+            post_md = open(file_name, 'w+')
             post_md.write('---\ntitle: ' + '\"' + title.decode('utf-8').replace('\"', '\\\"') + '\"' + '\ndate: ' + date + "\nurl: " + url.decode('utf-8') + '\n---\n\n' + content.decode('utf-8').replace('  <', ' <'))
+            get_images(soup, target_folder, file_name, weebly)
             post_md.close()
 
 
-if __name__ == '__main__':
+def get_images(soup, root_folder, post_file_name, weebly):
+    folder_name = "{}/{}".format(root_folder, post_file_name.split("blog__")[1].split(".")[0])
+    os.makedirs(folder_name)
+    images = []
+    for img in soup.findAll('img'):
+        src = img.get('src')
+        url_img = "http:{}".format(src) if src.startswith("//") else "{}{}".format(weebly, src) if src.startswith("/") else src
+        filename = url_img.split('/')[-1]
+        print("writing image {} ...".format(url_img))
+        urllib.request.urlretrieve(url_img, "{}/{}".format(folder_name, filename))
 
+
+if __name__ == '__main__':
+    os.system("clear")
+    print("Scrapping {}\n".format(sys.argv[1]))
     if len(sys.argv) != 3:
-        print("\nUsage: `" + sys.argv[0] + " <Weebly URL posts\' page> <Number of pages containing posts>`\n")
+        print("\nUsage: `" + sys.argv[0] + " <Weebly URL posts\' page>   <Target Folder\' page>`\n")
 
     else:
         WeeblyScraper().scrape(sys.argv[1], sys.argv[2])
